85c3d9cfbded5f04cde1caffeddb49466bc63325
laasya <laasyanakka@gmail.com>
2026-06-30 15:25:12 +0530

    Day 1: Pattern Printing

---
+++ golden_rule.py
@@ -0,0 +1,27 @@
+Imagine you have a notebook
+Each line is Row
+each thing you print in that line is column 
+
+example:
+*****
+*****
+think like this 
+Row 1->Print 5 stars
+Row 2->Print 5 stars
+
+so every pattern always haves 
+outer loop->Rows
+Inner loop->Columns
+
+Always Remember
+for row:
+    for column:
+        print something
+    move to next line
+print()    #It doesnt print anything it always says go to next line and without this everythinbg prints like (************)
+
+
+Before solving any pattern see 
+1.How many rows?
+2.How many things in Row?
+3.what should i print?
